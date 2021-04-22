09b5170e426e502f01be25fc9021999c64d445ad
Rajendra <rajendradanve@gmail.com>
2021-04-22 23:29:36 +0000

    working code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,23 @@
 
 let gameContainerWidth = $(".game-container").css("width");
-alert(gameContainerWidth);
-console.log(typeof (gameContainerWidth));
 
+$(".game-container").css("height", gameContainerWidth);
+
+let cellSize = parseFloat(gameContainerWidth.slice(0, -2))/7+"px";
+
+
+$(".column").css("width", cellSize);
+$(".cell").css("height", cellSize);
+
+let cellHeight = $(".cell").css("height");
+console.log(cellHeight);
 
 for(let i=0; i<7; i++){
+    $(".game-container").append(`<div id="column${i}"></div>`);
+    $(`#column${i}`).addClass("column");
+    for (let j=0; j<7; j++){
+        $(`#column${i}`).append(`<div id="cell${i}${j}"></div>`);
+        $(`#cell${i}${j}`).addClass("cell");
+    }
 
 }
